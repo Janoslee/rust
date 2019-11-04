af9f3fc5b28354371411f10fa340e31d095e0c74
Janos Lee <janoslee@gmail.com>
2019-11-04 22:32:22 +0000

    Guessing game final

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,35 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
+    let mut number_of_tries: u32 = 0;
 
-    println!("The secret number is: {}", secret_number);
+    //println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        number_of_tries = number_of_tries + 1;
 
-    let mut guess = String::new();
+        println!("Please input your guess. Try number: {}", number_of_tries);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line!");
+        let mut guess = String::new();
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read input");
 
-    println!("You guessed: {}", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You Win"),
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You Win in {} tries", number_of_tries);
+                break;
+            }
+        }
     }
 }
