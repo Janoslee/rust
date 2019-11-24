ae7bdced8773a88ecb48d0028009132b80181d60
Janos Lee <janoslee@gmail.com>
2019-11-24 00:38:13 +0000

    Just testin!

--- exercise2/src/main.rs
+++ exercise2/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 1;
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {}", y);
+    println!("The value of x is: {}", x);
 }
